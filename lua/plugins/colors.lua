227a8a82c54533adeb8f0088feed30243f3af541
Marvin Knoll <marvin.knoll@vertical-life.info>
2023-12-15 09:55:11 +0100

    higher contrast line numbers

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,5 @@
+return {
+  vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC' }),
+  vim.api.nvim_set_hl(0, 'LineNr', { fg='#51B3EC', bold=true }),
+  vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#51B3EC' }),
+}
