3570c766a4a08b6c7fdb0318e4d18d48ed42fe5e
marvinknoll <knollmarvin6@gmail.com>
2024-05-23 17:34:20 +0200

    some keymaps from primeagen

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,13 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    },
+  },
   defaults = {
     vimgrep_arguments = {
       -- 'rg',
@@ -13,4 +21,7 @@ return {
       "--hidden",
     },
   },
+  config = function()
+    require("telescope").load_extension("live_grep_args")
+  end,
 }
