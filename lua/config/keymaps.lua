630512c32c12e20813f2ca47b75b98cbfb24f3db
marvinknoll <knollmarvin6@gmail.com>
2024-05-23 17:34:19 +0200

    add keymaps to jump between windows

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.api.nvim_set_keymap("n", "<leader>wj", "<C-w>j", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>wk", "<C-w>k", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>wh", "<C-w>h", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>wl", "<C-w>l", { noremap = true })
+
