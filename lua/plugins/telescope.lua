02abdaa4dbff567f7fb7a141f567cf3957b6366d
marvinknoll <knollmarvin6@gmail.com>
2024-05-23 17:34:19 +0200

    telescope config

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  defaults = {
+    vimgrep_arguments = {
+      -- 'rg',
+      -- '--color=never',
+      -- '--no-heading',
+      -- '--with-filename',
+      -- '--line-number',
+      -- '--column',
+      -- '--smart-case',
+      "--no-ignore-vcs",
+      "--hidden",
+    },
+  },
+}
