b6119d2b55e00f51670ea917aef864da296999b7
marvinknoll <knollmarvin6@gmail.com>
2024-05-23 17:34:20 +0200

    jump to last edit position on opening file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Jump to last edit position on opening file
+vim.api.nvim_create_autocmd("BufReadPost", {
+  desc = "Open file at the last position it was edited earlier",
+  group = misc_augroup,
+  pattern = "*",
+  command = 'silent! normal! g`"zv',
+})
