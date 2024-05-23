38062172a7f92f92ed0baff96fb28ce7696f28a2
marvinknoll <knollmarvin6@gmail.com>
2024-05-23 17:34:19 +0200

    add some primeagen keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,11 @@ vim.api.nvim_set_keymap("n", "<leader>wk", "<C-w>k", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>wh", "<C-w>h", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>wl", "<C-w>l", { noremap = true })
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- yank into system clipblard with leader y and leader Y
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
