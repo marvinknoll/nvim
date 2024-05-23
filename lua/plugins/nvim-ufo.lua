3a647f222be0a0a6ea88d9d9b9aa434d5d3efb16
marvinknoll <knollmarvin6@gmail.com>
2024-05-23 17:34:20 +0200

    updates

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -33,7 +33,15 @@ return {
   end,
   config = function()
     require("ufo").setup({
-      close_fold_kinds = { "imports" },
+      close_fold_kinds_for_ft = {
+        description = [[After the buffer is displayed (opened for the first time), close the
+                    folds whose range with `kind` field is included in this option. For now,
+                    'lsp' provider's standardized kinds are 'comment', 'imports' and 'region'.
+                    This option is a table with filetype as key and fold kinds as value. Use a
+                    default value if value of filetype is absent.
+                    Run `UfoInspect` for details if your provider has extended the kinds.]],
+        default = { default = {} },
+      },
     })
 
     vim.api.nvim_create_autocmd("LspAttach", {
